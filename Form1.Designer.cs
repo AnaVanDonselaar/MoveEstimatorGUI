f143c5637f210d391feffd363322192bf36318a0
Ana Van Donselaar <127625377+AnaVanDonselaar@users.noreply.github.com>
2023-03-11 20:33:53 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MoveEstimatorGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInHours = new System.Windows.Forms.TextBox();
+            this.txtInMiles = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Estimated hours:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estimated miles:";
+            // 
+            // txtInHours
+            // 
+            this.txtInHours.Location = new System.Drawing.Point(184, 44);
+            this.txtInHours.Name = "txtInHours";
+            this.txtInHours.Size = new System.Drawing.Size(100, 20);
+            this.txtInHours.TabIndex = 2;
+            // 
+            // txtInMiles
+            // 
+            this.txtInMiles.Location = new System.Drawing.Point(185, 80);
+            this.txtInMiles.Name = "txtInMiles";
+            this.txtInMiles.Size = new System.Drawing.Size(100, 20);
+            this.txtInMiles.TabIndex = 3;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(400, 62);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(184, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Estimate cost:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(263, 170);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(100, 20);
+            this.txtOutput.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtInMiles);
+            this.Controls.Add(this.txtInHours);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Move Estimator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInHours;
+        private System.Windows.Forms.TextBox txtInMiles;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoveEstimatorGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            const double BASE_RATE = 200;
+            const double HOUR_RATE = 150;
+            const double MILE_RATE = 2;
+            double inputHours;
+            double inputMiles;
+            double calculatedEstimate;
+
+            Double.TryParse(txtInHours.Text, out inputHours);
+            Double.TryParse(txtInMiles.Text, out inputMiles);
+
+            calculatedEstimate = BASE_RATE + (inputHours * HOUR_RATE) + (inputMiles * MILE_RATE);
+
+            txtOutput.Text = calculatedEstimate.ToString();
+        }
+    }
+}
